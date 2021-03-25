1d3147830a05912aba5875e1712ee38c5c4f416f
alex-stytch <alex@stytch.com>
2021-03-25 10:36:14 -0700

    Update AuthenticateMagicLinkResponse

--- types/index.d.ts
+++ types/index.d.ts
@@ -159,6 +159,7 @@ declare module 'stytch' {
     interface AuthenticateMagicLinkResponse extends BaseResponse {
         user_id: string;
         email_id: string;
+        method_id: string;
     }
 
     interface RevokePendingInviteRequest {
