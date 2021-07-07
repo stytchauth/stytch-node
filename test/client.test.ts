67ae727ca96ce208432cdd5791dc884dc16536b3
Jeremy Kaplan <jeremy@stytch.com>
2021-07-07 15:21:09 -0700

    Fix env string in client tests

--- test/client.test.ts
+++ test/client.test.ts
@@ -12,7 +12,7 @@ describe("config errors", () => {
       new stytch.Client({
         project_id: "",
         secret: "secret-test-11111111-1111-4111-8111-111111111111",
-        env: "test",
+        env: stytch.envs.test,
       });
     }).toThrow(/Missing "project_id" in config/);
   });
@@ -22,7 +22,7 @@ describe("config errors", () => {
       new stytch.Client({
         project_id: "project-test-00000000-0000-4000-8000-000000000000",
         secret: "",
-        env: "test",
+        env: stytch.envs.test,
       });
     }).toThrow(/Missing "secret" in config/);
   });
@@ -44,7 +44,7 @@ describe("backward compatibility", () => {
     const client = new stytch.Client({
       project_id: "project-test-00000000-0000-4000-8000-000000000000",
       secret: "secret-test-11111111-1111-4111-8111-111111111111",
-      env: "test",
+      env: stytch.envs.test,
     });
 
     expect(client.createUser).toBe(client.users.create);
