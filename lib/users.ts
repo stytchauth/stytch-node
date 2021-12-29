a5485b96d49ad5dc843fb71b2af767261312b2e0
Maxwell Gerber <max@stytch.com>
2021-12-28 22:11:32 -0800

    update for filter_name/filter_value syntax

--- lib/users.ts
+++ lib/users.ts
@@ -7,7 +7,7 @@ import type {
   Email,
   Name,
   PhoneNumber,
-  WebAuthnRegistration,
+  WebAuthnRegistration
 } from "./shared";
 
 export type UserID = string;
@@ -58,74 +58,80 @@ export enum UserSearchOperator {
 
 export type UserSearchOperand =
   | {
-      filter: "created_at_greater_than";
-      // Timestamp in RFC 3339 Format
-      created_at_greater_than: string;
-    }
+  filter_name: "created_at_greater_than";
+  // Timestamp in RFC 3339 Format
+  filter_value: string;
+}
   | {
-      filter: "created_at_less_than";
-      // Timestamp in RFC 3339 Format
-      created_at_less_than: string;
-    }
+  filter_name: "created_at_less_than";
+  // Timestamp in RFC 3339 Format
+  filter_value: string;
+}
   | {
-      filter: "created_at_between";
-      // Timestamp in RFC 3339 Format
-      created_at_greater_than: string;
-      // Timestamp in RFC 3339 Format
-      created_at_less_than: string;
-    }
+  filter_name: "created_at_between";
+  filter_value: {
+    // Timestamp in RFC 3339 Format
+    greater_than: string;
+    // Timestamp in RFC 3339 Format
+    less_than: string;
+  }
+}
   | {
-      filter: "status";
-      status: "active" | "pending";
-    }
+  filter_name: "status";
+  filter_value: "active" | "pending";
+}
   | {
-      filter: "oauth_provider";
-      oauth_providers: string[];
-    }
+  filter_name: "oauth_provider";
+  filter_value: string[];
+}
   | {
-      filter: "user_id";
-      user_ids: string[];
-    }
+  filter_name: "user_id";
+  filter_value: string[];
+}
   | {
-      filter: "full_name_fuzzy";
-      full_name_fuzzy: string;
-    }
+  filter_name: "full_name_fuzzy";
+  filter_value: string;
+}
   | {
-      filter: "phone_number";
-      phone_numbers: string[];
-    }
+  filter_name: "phone_number";
+  filter_value: string[];
+}
   | {
-      filter: "phone_id";
-      phone_ids: string[];
-    }
+  filter_name: "phone_id";
+  filter_value: string[];
+}
   | {
-      filter: "phone_verified";
-      phone_verified: boolean;
-    }
+  filter_name: "phone_verified";
+  filter_value: boolean;
+}
   | {
-      filter: "phone_number_fuzzy";
-      phone_number_fuzzy: string;
-    }
+  filter_name: "phone_number_fuzzy";
+  filter_value: string;
+}
   | {
-      filter: "email_address";
-      email_addresses: string[];
-    }
+  filter_name: "email_address";
+  filter_value: string[];
+}
   | {
-      filter: "email_id";
-      email_ids: string[];
-    }
+  filter_name: "email_id";
+  filter_value: string[];
+}
   | {
-      filter: "email_verified";
-      email_verified: boolean;
-    }
+  filter_name: "email_verified";
+  filter_value: boolean;
+}
   | {
-      filter: "email_address_fuzzy";
-      email_address_fuzzy: string;
-    }
+  filter_name: "email_address_fuzzy";
+  filter_value: string;
+}
   | {
-      filter: "webauthn_registration_verified";
-      webauthn_registration_verified: boolean;
-    };
+  filter_name: "webauthn_registration_verified";
+  filter_value: boolean;
+}
+  | {
+  filter_name: "webauthn_registration_id";
+  filter_value: string[];
+};
 
 export interface SearchRequest {
   limit?: number;
@@ -210,7 +216,7 @@ export class UserSearchIterator {
     const res = await this.client.search(this.data);
     this.data = {
       ...this.data,
-      cursor: res.results_metadata.next_cursor,
+      cursor: res.results_metadata.next_cursor
     };
     if (!this.data.cursor) {
       this.mode = mode.complete;
@@ -224,7 +230,7 @@ export class UserSearchIterator {
     return this.mode !== mode.complete;
   }
 
-  async *[Symbol.asyncIterator](): AsyncIterator<User[]> {
+  async* [Symbol.asyncIterator](): AsyncIterator<User[]> {
     while (this.hasNext()) {
       yield this.next();
     }
@@ -247,17 +253,17 @@ export class Users {
     return request(this.client, {
       method: "POST",
       url: this.base_path,
-      data,
+      data
     });
   }
 
   get(userID: UserID): Promise<GetResponse> {
     return request<BaseResponse & UserRaw>(this.client, {
       method: "GET",
-      url: this.endpoint(userID),
+      url: this.endpoint(userID)
     }).then((res) => ({
       ...res,
-      ...parseUser(res),
+      ...parseUser(res)
     }));
   }
 
@@ -265,11 +271,11 @@ export class Users {
     return request<SearchResponseRaw>(this.client, {
       method: "POST",
       url: this.endpoint("search"),
-      data,
+      data
     }).then((res) => {
       return {
         ...res,
-        results: res.results.map(parseUser),
+        results: res.results.map(parseUser)
       };
     });
   }
@@ -282,14 +288,14 @@ export class Users {
     return request(this.client, {
       method: "PUT",
       url: this.endpoint(userID),
-      data,
+      data
     });
   }
 
   delete(userID: UserID): Promise<DeleteResponse> {
     return request(this.client, {
       method: "DELETE",
-      url: this.endpoint(userID),
+      url: this.endpoint(userID)
     });
   }
 
@@ -297,21 +303,21 @@ export class Users {
     return request(this.client, {
       method: "GET",
       url: this.endpoint("pending"),
-      params,
+      params
     });
   }
 
   deleteEmail(emailID: string): Promise<DeleteEmailResponse> {
     return request(this.client, {
       method: "DELETE",
-      url: this.endpoint(`emails/${emailID}`),
+      url: this.endpoint(`emails/${emailID}`)
     });
   }
 
   deletePhoneNumber(phoneID: string): Promise<DeletePhoneNumberResponse> {
     return request(this.client, {
       method: "DELETE",
-      url: this.endpoint(`phone_numbers/${phoneID}`),
+      url: this.endpoint(`phone_numbers/${phoneID}`)
     });
   }
 
@@ -320,7 +326,7 @@ export class Users {
   ): Promise<DeleteWebAuthnRegistrationResponse> {
     return request(this.client, {
       method: "DELETE",
-      url: this.endpoint(`webauthn_registrations/${webAuthnRegistrationID}`),
+      url: this.endpoint(`webauthn_registrations/${webAuthnRegistrationID}`)
     });
   }
 }
@@ -329,6 +335,6 @@ function parseUser(user: UserRaw): User {
   console.log(user);
   return {
     ...user,
-    created_at: new Date(user.created_at),
+    created_at: new Date(user.created_at)
   };
 }
