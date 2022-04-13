994edbdfaa7a259ff58c9b76e6c774f048108dee
austin-stytch <101587420+austin-stytch@users.noreply.github.com>
2022-04-13 15:08:49 -0600

    austin/gitlab: support gitlab as an oauth type (#117)

--- types/lib/shared.d.ts
+++ types/lib/shared.d.ts
@@ -96,6 +96,16 @@ export interface GithubOAuthFactor {
         provider_subject: string;
     };
 }
+export interface GitLabOAuthFactor {
+    delivery_method: "oauth_gitlab";
+    type: string;
+    last_authenticated_at: string;
+    gitlab_oauth_factor: {
+        id: string;
+        email_id: string;
+        provider_subject: string;
+    };
+}
 export interface FacebookOAuthFactor {
     delivery_method: "oauth_facebook";
     type: string;
@@ -142,7 +152,7 @@ export interface CryptoWalletFactor {
         crypto_wallet_type: string;
     };
 }
-export declare type AuthenticationFactor = EmailFactor | PhoneNumberFactor | GoogleOAuthFactor | MicrosoftOAuthFactor | AppleOAuthFactor | GithubOAuthFactor | FacebookOAuthFactor | WebAuthnFactor | AuthenticatorAppFactor | RecoveryCodeFactor | CryptoWalletFactor;
+export declare type AuthenticationFactor = EmailFactor | PhoneNumberFactor | GoogleOAuthFactor | MicrosoftOAuthFactor | AppleOAuthFactor | GithubOAuthFactor | GitLabOAuthFactor | FacebookOAuthFactor | WebAuthnFactor | AuthenticatorAppFactor | RecoveryCodeFactor | CryptoWalletFactor;
 export interface Session {
     session_id: string;
     user_id: string;
