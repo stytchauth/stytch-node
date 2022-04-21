f359710442001842d282e14b753a7b35e2b0c804
Danny Thomson <dthomson25@gmail.com>
2022-04-20 19:33:24 -0700

    Add Stytch and IdP dual support

--- types/lib/oauth.d.ts
+++ types/lib/oauth.d.ts
@@ -1,27 +1,25 @@
 import type { AxiosInstance } from "axios";
 import type { BaseResponse, Session } from "./shared";
 export interface AuthenticateRequest {
-    session_management_type?: "stytch" | "idp" | "none";
     session_token?: string;
     session_jwt?: string;
     session_duration_minutes?: number;
-}
-export interface OAuthSession {
-    idp?: {
-        access_token?: string;
-        refresh_token?: string;
-    };
-    stytch_session?: {
-        session: Session;
-        session_token: string;
-        session_jwt: string;
-    };
 }
 export interface AuthenticateResponse extends BaseResponse {
     user_id: string;
     provider_subject: string;
     provider_type: string;
-    session?: OAuthSession;
+    session_token?: string;
+    session_jwt?: string;
+    session?: Session;
+    providerValues: ProvidersValues;
+}
+export interface ProvidersValues {
+    access_token?: string;
+    refresh_token?: string;
+    id_token?: string;
+    expires_at?: number;
+    scopes: string[];
 }
 export declare class OAuth {
     base_path: string;
