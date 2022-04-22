8f473f091adb02e4acf37f6d1a70de581d5ab9a1
Danny Thomson <70562257+danny-stytch@users.noreply.github.com>
2022-04-22 12:00:27 -0700

    Add Stytch and IdP sessions at the same time (#119)

--- lib/client.ts
+++ lib/client.ts
@@ -12,9 +12,6 @@ import { Sessions } from "./sessions";
 import { TOTPs } from "./totps";
 import { WebAuthn } from "./webauthn";
 import type { AxiosInstance } from "axios";
-import type * as users from "./users";
-import type * as magicLinks from "./magic_links";
-import type * as otps from "./otps";
 
 const DEFAULT_TIMEOUT = 10 * 60 * 1000; // Ten minutes
 
@@ -96,104 +93,5 @@ export class Client {
     this.totps = new TOTPs(this.client);
     this.webauthn = new WebAuthn(this.client);
     this.cryptoWallets = new CryptoWallets(this.client);
-  }
-
-  /** @deprecated since version 3.0. Will be deleted in version 4.0.  Use users.create instead. */
-  createUser(request: users.CreateRequest): Promise<users.CreateResponse> {
-    return this.users.create(request);
-  }
-
-  /** @deprecated since version 3.0. Will be deleted in version 4.0.  Use users.get instead. */
-  getUser(userID: users.UserID): Promise<users.GetResponse> {
-    return this.users.get(userID);
-  }
-
-  /** @deprecated since version 3.0. Will be deleted in version 4.0.  Use users.update instead. */
-  updateUser(
-    userID: users.UserID,
-    request: users.UpdateRequest
-  ): Promise<users.UpdateResponse> {
-    return this.users.update(userID, request);
-  }
-
-  /** @deprecated since version 3.0. Will be deleted in version 4.0.  Use users.delete instead. */
-  deleteUser(userID: users.UserID): Promise<users.DeleteResponse> {
-    return this.users.delete(userID);
-  }
-
-  /** @deprecated since version 3.0. Will be deleted in version 4.0.  Use users.deleteEmail instead. */
-  deleteUserEmail(emailID: string): Promise<users.DeleteEmailResponse> {
-    return this.users.deleteEmail(emailID);
-  }
-
-  /** @deprecated since version 3.0. Will be deleted in version 4.0.  Use users.deletePhoneNumber instead. */
-  deleteUserPhoneNumber(
-    phoneID: string
-  ): Promise<users.DeletePhoneNumberResponse> {
-    return this.users.deletePhoneNumber(phoneID);
-  }
-
-  /** @deprecated since version 3.0. Will be deleted in version 4.0.  Use users.getPending instead. */
-  getPendingUsers(
-    request?: users.GetPendingRequest
-  ): Promise<users.GetPendingResponse> {
-    return this.users.getPending(request);
-  }
-
-  /** @deprecated since version 3.0. Will be deleted in version 4.0.  Use magicLinks.email.send instead. */
-  sendMagicLinkByEmail(
-    data: magicLinks.SendByEmailRequest
-  ): Promise<magicLinks.SendByEmailResponse> {
-    return this.magicLinks.email.send(data);
-  }
-
-  /** @deprecated since version 3.0. Will be deleted in version 4.0.  Use magicLinks.email.loginOrCreate instead. */
-  loginOrCreate(
-    data: magicLinks.LoginOrCreateByEmailRequest
-  ): Promise<magicLinks.LoginOrCreateByEmailResponse> {
-    return this.magicLinks.email.loginOrCreate(data);
-  }
-
-  /** @deprecated since version 3.0. Will be deleted in version 4.0.  Use magicLinks.email.invite instead. */
-  inviteByEmail(
-    data: magicLinks.InviteByEmailRequest
-  ): Promise<magicLinks.InviteByEmailResponse> {
-    return this.magicLinks.email.invite(data);
-  }
-
-  /** @deprecated since version 3.0. Will be deleted in version 4.0.  Use magicLinks.email.revokeInvite instead. */
-  revokePendingInvite(
-    data: magicLinks.RevokePendingInviteByEmailRequest
-  ): Promise<magicLinks.RevokePendingInviteByEmailResponse> {
-    return this.magicLinks.email.revokeInvite(data);
-  }
-
-  /** @deprecated since version 3.0. Will be deleted in version 4.0.  Use magicLinks.authenticate instead. */
-  authenticateMagicLink(
-    token: string,
-    data?: magicLinks.AuthenticateRequest
-  ): Promise<magicLinks.AuthenticateResponse> {
-    return this.magicLinks.authenticate(token, data);
-  }
-
-  /** @deprecated since version 3.0. Will be deleted in version 4.0.  Use otps.sms.send instead. */
-  sendOTPBySMS(
-    data: otps.SendOTPBySMSRequest
-  ): Promise<otps.SendOTPBySMSResponse> {
-    return this.otps.sms.send(data);
-  }
-
-  /** @deprecated since version 3.0. Will be deleted in version 4.0.  Use otps.sms.loginOrCreate instead. */
-  loginOrCreateUserBySMS(
-    data: otps.LoginOrCreateUserBySMSRequest
-  ): Promise<otps.LoginOrCreateUserBySMSResponse> {
-    return this.otps.sms.loginOrCreate(data);
-  }
-
-  /** @deprecated since version 3.0. Will be deleted in version 4.0.  Use otps.authenticate instead. */
-  authenticateOTP(
-    data: otps.AuthenticateRequest
-  ): Promise<otps.AuthenticateResponse> {
-    return this.otps.authenticate(data);
   }
 }
--- types/lib/client.d.ts
+++ types/lib/client.d.ts
@@ -6,9 +6,6 @@ import { OTPs } from "./otps";
 import { Sessions } from "./sessions";
 import { TOTPs } from "./totps";
 import { WebAuthn } from "./webauthn";
-import type * as users from "./users";
-import type * as magicLinks from "./magic_links";
-import type * as otps from "./otps";
 interface Config {
     project_id: string;
     secret: string;
@@ -26,35 +23,5 @@ export declare class Client {
     cryptoWallets: CryptoWallets;
     private client;
     constructor(config: Config);
-    /** @deprecated since version 3.0. Will be deleted in version 4.0.  Use users.create instead. */
-    createUser(request: users.CreateRequest): Promise<users.CreateResponse>;
-    /** @deprecated since version 3.0. Will be deleted in version 4.0.  Use users.get instead. */
-    getUser(userID: users.UserID): Promise<users.GetResponse>;
-    /** @deprecated since version 3.0. Will be deleted in version 4.0.  Use users.update instead. */
-    updateUser(userID: users.UserID, request: users.UpdateRequest): Promise<users.UpdateResponse>;
-    /** @deprecated since version 3.0. Will be deleted in version 4.0.  Use users.delete instead. */
-    deleteUser(userID: users.UserID): Promise<users.DeleteResponse>;
-    /** @deprecated since version 3.0. Will be deleted in version 4.0.  Use users.deleteEmail instead. */
-    deleteUserEmail(emailID: string): Promise<users.DeleteEmailResponse>;
-    /** @deprecated since version 3.0. Will be deleted in version 4.0.  Use users.deletePhoneNumber instead. */
-    deleteUserPhoneNumber(phoneID: string): Promise<users.DeletePhoneNumberResponse>;
-    /** @deprecated since version 3.0. Will be deleted in version 4.0.  Use users.getPending instead. */
-    getPendingUsers(request?: users.GetPendingRequest): Promise<users.GetPendingResponse>;
-    /** @deprecated since version 3.0. Will be deleted in version 4.0.  Use magicLinks.email.send instead. */
-    sendMagicLinkByEmail(data: magicLinks.SendByEmailRequest): Promise<magicLinks.SendByEmailResponse>;
-    /** @deprecated since version 3.0. Will be deleted in version 4.0.  Use magicLinks.email.loginOrCreate instead. */
-    loginOrCreate(data: magicLinks.LoginOrCreateByEmailRequest): Promise<magicLinks.LoginOrCreateByEmailResponse>;
-    /** @deprecated since version 3.0. Will be deleted in version 4.0.  Use magicLinks.email.invite instead. */
-    inviteByEmail(data: magicLinks.InviteByEmailRequest): Promise<magicLinks.InviteByEmailResponse>;
-    /** @deprecated since version 3.0. Will be deleted in version 4.0.  Use magicLinks.email.revokeInvite instead. */
-    revokePendingInvite(data: magicLinks.RevokePendingInviteByEmailRequest): Promise<magicLinks.RevokePendingInviteByEmailResponse>;
-    /** @deprecated since version 3.0. Will be deleted in version 4.0.  Use magicLinks.authenticate instead. */
-    authenticateMagicLink(token: string, data?: magicLinks.AuthenticateRequest): Promise<magicLinks.AuthenticateResponse>;
-    /** @deprecated since version 3.0. Will be deleted in version 4.0.  Use otps.sms.send instead. */
-    sendOTPBySMS(data: otps.SendOTPBySMSRequest): Promise<otps.SendOTPBySMSResponse>;
-    /** @deprecated since version 3.0. Will be deleted in version 4.0.  Use otps.sms.loginOrCreate instead. */
-    loginOrCreateUserBySMS(data: otps.LoginOrCreateUserBySMSRequest): Promise<otps.LoginOrCreateUserBySMSResponse>;
-    /** @deprecated since version 3.0. Will be deleted in version 4.0.  Use otps.authenticate instead. */
-    authenticateOTP(data: otps.AuthenticateRequest): Promise<otps.AuthenticateResponse>;
 }
 export {};
