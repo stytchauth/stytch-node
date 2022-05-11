814e22796e51ff67b06ab1321a4ce7f1a99494d7
mia-stytch <98918730+mia-stytch@users.noreply.github.com>
2022-05-11 12:48:59 -0700

    Add Profile Picture and Locale to OAuthProvider (#128)

--- lib/shared.ts
+++ lib/shared.ts
@@ -52,6 +52,8 @@ export interface CryptoWallet {
 export interface OAuthProvider {
   provider_subject: string;
   provider_type: string;
+  profile_picture_url: string;
+  locale: string;
 }
 
 export interface EmailFactor {
