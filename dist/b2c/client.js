6200287c911d1c93fcfb6ec0474333487cec15a2
Maxwell Gerber <max@stytch.com>
2023-03-20 14:32:35 -0700

    fix: Use B2B JWKS endpoint for B2B projects

--- dist/b2c/client.js
+++ dist/b2c/client.js
@@ -25,9 +25,21 @@ var _webauthn = require("./webauthn");
 
 var _client = require("../shared/client");
 
+var jose = _interopRequireWildcard(require("jose"));
+
+function _getRequireWildcardCache(nodeInterop) { if (typeof WeakMap !== "function") return null; var cacheBabelInterop = new WeakMap(); var cacheNodeInterop = new WeakMap(); return (_getRequireWildcardCache = function (nodeInterop) { return nodeInterop ? cacheNodeInterop : cacheBabelInterop; })(nodeInterop); }
+
+function _interopRequireWildcard(obj, nodeInterop) { if (!nodeInterop && obj && obj.__esModule) { return obj; } if (obj === null || typeof obj !== "object" && typeof obj !== "function") { return { default: obj }; } var cache = _getRequireWildcardCache(nodeInterop); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (key !== "default" && Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj.default = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
+
 class Client extends _client.BaseClient {
   constructor(config) {
     super(config);
+    this.jwtConfig = {
+      // Only allow JWTs that were meant for this project.
+      projectID: config.project_id,
+      // Fetch the signature verification keys for this project as needed.
+      jwks: jose.createRemoteJWKSet(new URL(`sessions/jwks/${config.project_id}`, this.fetchConfig.baseURL))
+    };
     this.users = new _users.Users(this.fetchConfig);
     this.magicLinks = new _magic_links.MagicLinks(this.fetchConfig);
     this.oauth = new _oauth.OAuth(this.fetchConfig);
