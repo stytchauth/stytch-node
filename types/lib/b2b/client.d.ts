a7cfbe172b2e19980866f5203e6f290ed049dc38
Taronish Daruwalla <taronish@stytch.com>
2023-03-31 13:35:01 -0700

    add to client

--- types/lib/b2b/client.d.ts
+++ types/lib/b2b/client.d.ts
@@ -5,6 +5,7 @@ import { SSO } from "./sso";
 import { BaseClient, ClientConfig } from "../shared/client";
 import { JwtConfig } from "../shared/sessions";
 import { Discovery } from "./discovery";
+import { Passwords } from "./passwords";
 export declare class B2BClient extends BaseClient {
     protected jwtConfig: JwtConfig;
     magicLinks: MagicLinks;
@@ -12,5 +13,6 @@ export declare class B2BClient extends BaseClient {
     organizations: Organizations;
     sso: SSO;
     discovery: Discovery;
+    passwords: Passwords;
     constructor(config: ClientConfig);
 }
