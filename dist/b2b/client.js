6f16b44288c2f8284d7c6ebf660dee3043b03b9b
Vincent Jin <vincent@stytch.com>
2023-06-07 07:08:12 -0700

    dist

--- dist/b2b/client.js
+++ dist/b2b/client.js
@@ -21,6 +21,8 @@ var _discovery = require("./discovery");
 
 var _passwords = require("./passwords");
 
+var _oauth = require("./oauth");
+
 function _getRequireWildcardCache(nodeInterop) { if (typeof WeakMap !== "function") return null; var cacheBabelInterop = new WeakMap(); var cacheNodeInterop = new WeakMap(); return (_getRequireWildcardCache = function (nodeInterop) { return nodeInterop ? cacheNodeInterop : cacheBabelInterop; })(nodeInterop); }
 
 function _interopRequireWildcard(obj, nodeInterop) { if (!nodeInterop && obj && obj.__esModule) { return obj; } if (obj === null || typeof obj !== "object" && typeof obj !== "function") { return { default: obj }; } var cache = _getRequireWildcardCache(nodeInterop); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (key !== "default" && Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj.default = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
@@ -41,6 +43,7 @@ class B2BClient extends _client.BaseClient {
     };
     this.magicLinks = new _magic_links.MagicLinks(this.fetchConfig);
     this.sessions = new _sessions.Sessions(this.fetchConfig, this.jwtConfig);
+    this.oauth = new _oauth.OAuth(this.fetchConfig);
     this.organizations = new _organizations.Organizations(this.fetchConfig);
     this.sso = new _sso.SSO(this.fetchConfig);
     this.discovery = new _discovery.Discovery(this.fetchConfig);
--- types/lib/b2b/client.d.ts
+++ types/lib/b2b/client.d.ts
@@ -6,10 +6,12 @@ import { BaseClient, ClientConfig } from "../shared/client";
 import { JwtConfig } from "../shared/sessions";
 import { Discovery } from "./discovery";
 import { Passwords } from "./passwords";
+import { OAuth } from "./oauth";
 export declare class B2BClient extends BaseClient {
     protected jwtConfig: JwtConfig;
     magicLinks: MagicLinks;
     sessions: Sessions;
+    oauth: OAuth;
     organizations: Organizations;
     sso: SSO;
     discovery: Discovery;
