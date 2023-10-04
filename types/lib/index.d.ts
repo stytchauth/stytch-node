386f279d345baea6bd3e900255b9c179b392dedb
Maxwell Gerber <max@stytch.com>
2023-10-04 12:13:13 -0700

    regen

--- types/lib/index.d.ts
+++ types/lib/index.d.ts
@@ -1,12 +1,12 @@
 import { Client } from "./b2c/client";
 import { B2BClient } from "./b2b/client";
 export { Client, B2BClient };
+export * from "./b2c/index";
+export * from "./b2b/index";
+export * as envs from "./shared/envs";
+export * from "./shared/errors";
 declare const _default: {
     Client: typeof Client;
     B2BClient: typeof B2BClient;
 };
 export default _default;
-export * from "./b2c/index";
-export * from "./b2b/index";
-export * as envs from "./shared/envs";
-export * from "./shared/errors";
