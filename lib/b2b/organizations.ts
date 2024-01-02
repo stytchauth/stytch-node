7c4b145f4a02b5c596dfb6cd56b5c4d0c0943661
Ollie Elmgren <111321901+ollie-stytch@users.noreply.github.com>
2024-01-02 16:34:42 -0600

    Bump Version +  B2B TOTP MFA cont. (#290)

--- lib/b2b/organizations.ts
+++ lib/b2b/organizations.ts
@@ -499,7 +499,23 @@ export interface B2BOrganizationsCreateRequest {
    *   for more information about role assignment.
    */
   rbac_email_implicit_role_assignments?: EmailImplicitRoleAssignment[];
+  /**
+   * The setting that controls which mfa methods can be used by Members of an Organization. The accepted
+   * values are:
+   *
+   *   `ALL_ALLOWED` – the default setting which allows all authentication methods to be used.
+   *
+   *   `RESTRICTED` – only methods that comply with `allowed_auth_methods` can be used for authentication.
+   * This setting does not apply to Members with `is_breakglass` set to `true`.
+   *
+   */
   mfa_methods?: string;
+  /**
+   * An array of allowed mfa authentication methods. This list is enforced when `mfa_methods` is set to
+   * `RESTRICTED`.
+   *   The list's accepted values are: `sms_otp` and `totp`.
+   *
+   */
   allowed_mfa_methods?: string[];
 }
 
@@ -786,7 +802,31 @@ export interface B2BOrganizationsUpdateRequest {
    * permission to perform the `update.settings.implicit-roles` action on the `stytch.organization` Resource.
    */
   rbac_email_implicit_role_assignments?: string[];
+  /**
+   * The setting that controls which mfa methods can be used by Members of an Organization. The accepted
+   * values are:
+   *
+   *   `ALL_ALLOWED` – the default setting which allows all authentication methods to be used.
+   *
+   *   `RESTRICTED` – only methods that comply with `allowed_auth_methods` can be used for authentication.
+   * This setting does not apply to Members with `is_breakglass` set to `true`.
+   *
+   *
+   * If this field is provided and a session header is passed into the request, the Member Session must have
+   * permission to perform the `update.settings.allowed-auth-methods` action on the `stytch.organization`
+   * Resource.
+   */
   mfa_methods?: string;
+  /**
+   * An array of allowed mfa authentication methods. This list is enforced when `mfa_methods` is set to
+   * `RESTRICTED`.
+   *   The list's accepted values are: `sms_otp` and `totp`.
+   *
+   *
+   * If this field is provided and a session header is passed into the request, the Member Session must have
+   * permission to perform the `update.settings.allowed-mfa-methods` action on the `stytch.organization`
+   * Resource.
+   */
   allowed_mfa_methods?: string[];
 }
 
