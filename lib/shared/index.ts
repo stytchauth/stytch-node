aa5eecb01a479dfa13d530216dbca0fe59c84061
Maxwell Gerber <max@stytch.com>
2024-09-04 11:37:49 -0700

    pr comments

--- lib/shared/index.ts
+++ lib/shared/index.ts
@@ -41,10 +41,8 @@ export async function request<T>(
     response = await fetch(url.toString(), {
       method: requestConfig.method,
       body: body,
-      // [AUTH-2047] things fail catastrophically when using the NextJS fetch-cache
-      // so we need to explicitly opt out of it using the "no-store" tag
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      // @ts-ignore
+      // @ts-expect-error [AUTH-2047] things fail catastrophically when using the NextJS fetch-cache
+      // so we need to explicitly opt out of it using the "no-store" tag - which isn't part of the core Node fetch API
       cache: "no-store",
       ...fetchConfig,
       headers: finalHeaders,
