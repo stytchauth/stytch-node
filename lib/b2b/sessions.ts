978ab01fb1e7b6e8d4ac425d665b3aaeffc2e352
ci-stytch <72180218+ci-stytch@users.noreply.github.com>
2025-02-03 11:54:59 -0800

    Add French & Italian Locales (#357)
    Co-authored-by: Stytch Codegen Bot <support@stytch.com>

--- lib/b2b/sessions.ts
+++ lib/b2b/sessions.ts
@@ -245,7 +245,7 @@ export interface B2BSessionsExchangeRequest {
    * [here](https://docs.google.com/forms/d/e/1FAIpQLScZSpAu_m2AmLXRT3F3kap-s_mcV6UTBitYn6CdyWP0-o7YjQ/viewform?usp=sf_link")!
    *
    */
-  locale?: "en" | "es" | "pt-br" | string;
+  locale?: "en" | "es" | "pt-br" | "fr" | "it" | string;
 }
 
 // Response type for `sessions.exchange`.
