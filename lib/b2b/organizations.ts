e48c86a6bef41e1bb5f661d5abd0b650b8fa2628
Jennifer McCleary <jennifer@stytch.com>
2025-04-15 12:28:31 -0400

    Run prettier

--- lib/b2b/organizations.ts
+++ lib/b2b/organizations.ts
@@ -1177,7 +1177,7 @@ export type OrganizationSearchOperand =
   | {
       filter_name: "claimed_email_domains";
       filter_value: string[];
-  };
+    };
 
 export type MemberSearchOperand =
   | {
