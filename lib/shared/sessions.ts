4ed77db94ba7835ee065a79024abd54183206e54
Ollie Elmgren <ollie@stytch.com>
2025-04-23 15:04:09 -0700

    session update

--- lib/shared/sessions.ts
+++ lib/shared/sessions.ts
@@ -15,7 +15,10 @@ export interface JwtConfig {
 // We expect issuers with no trailing slash at the end but store the base URL with one.
 // We use this function to normalize this difference.
 export function trimTrailingSlash(baseURL: string): string {
-  return baseURL.replace(/\/+$/, "");
+  while (baseURL.endsWith('/')) {
+    baseURL = baseURL.slice(0, -1);
+  }
+  return baseURL;
 }
 
 type SessionClaim = {
