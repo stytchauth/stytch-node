786fe126879d808ccb9d2683fcfd03a9c9e67e85
Max Gerber <89937743+max-stytch@users.noreply.github.com>
2025-07-28 10:50:56 -0700

    checkin: Show shared implementation of RBAC across B2B and Consumer (#402)

--- lib/b2b/rbac_local.ts
+++ lib/b2b/rbac_local.ts
@@ -1,11 +1,15 @@
 // This file is manually generated!
 
-import { Policy, RBAC } from "./rbac";
+import { Policy } from "./rbac";
 import { AuthorizationCheck } from "./sessions";
 import { ClientError } from "../shared/errors";
-
-// We want to refresh if the policy is more than 5 minutes old
-const MAX_AGE_MS = 1000 * 60 * 5;
+import {
+  baseScopeAuthorizationCheckArgs,
+  performBaseRoleAuthorizationCheck,
+  performBaseScopeAuthorizationCheck,
+  BasePolicyCache,
+  baseRoleAuthorizationCheckArgs,
+} from "../shared/rbac_local";
 
 // PolicyCache handles local caching of RBAC policy information
 // It is based on https://github.com/panva/jose/blob/main/src/jwks/remote.ts
@@ -13,41 +17,14 @@ const MAX_AGE_MS = 1000 * 60 * 5;
 // and tracks the timestamp internally. this has a few _very nice_ properties:
 // - It works very well in Jest environments, since there's no boilerplate required to tear down the setInterval
 // - No work is done if RBAC is not used and the policy is not required
-export class PolicyCache {
-  private rbac: RBAC;
-  private _policy?: Policy;
-  private _timestamp?: number;
-
-  constructor(rbac: RBAC) {
-    this.rbac = rbac;
-  }
-
-  private fresh(): boolean {
-    return !!this._timestamp && Date.now() < this._timestamp + MAX_AGE_MS;
-  }
-
-  private async reload() {
-    const policyResponse = await this.rbac.policy();
-    this._policy = policyResponse.policy;
-    this._timestamp = Date.now();
-  }
-
-  async getPolicy(): Promise<Policy> {
-    if (!this._policy || !this.fresh()) {
-      await this.reload();
-    }
-    return this._policy as Policy;
-  }
-}
+export class PolicyCache extends BasePolicyCache<Policy> {}
 
 export function performAuthorizationCheck({
   policy,
   subjectRoles,
   subjectOrgID,
   authorizationCheck,
-}: {
-  policy: Policy;
-  subjectRoles: string[];
+}: Omit<baseRoleAuthorizationCheckArgs, "callerType"> & {
   subjectOrgID: string;
   authorizationCheck: AuthorizationCheck;
 }): void {
@@ -57,24 +34,12 @@ export function performAuthorizationCheck({
       "Member belongs to different organization"
     );
   }
-  const hasPermission = policy.roles
-    .filter((role) => subjectRoles.includes(role.role_id))
-    .flatMap((role) => role.permissions)
-    .some((permission) => {
-      const hasMatchingAction =
-        permission.actions.includes(authorizationCheck.action) ||
-        permission.actions.includes("*");
-      const hasMatchingResource =
-        authorizationCheck.resource_id === permission.resource_id;
-      return hasMatchingAction && hasMatchingResource;
-    });
-
-  if (!hasPermission) {
-    throw new ClientError(
-      "invalid_permissions",
-      "Member does not have permission to perform the requested action"
-    );
-  }
+  performBaseRoleAuthorizationCheck({
+    policy,
+    subjectRoles,
+    authorizationCheck,
+    callerType: "Member",
+  });
 }
 
 export function performScopeAuthorizationCheck({
@@ -82,9 +47,7 @@ export function performScopeAuthorizationCheck({
   tokenScopes,
   subjectOrgID,
   authorizationCheck,
-}: {
-  policy: Policy;
-  tokenScopes: string[];
+}: Omit<baseScopeAuthorizationCheckArgs, "callerType"> & {
   subjectOrgID: string;
   authorizationCheck: AuthorizationCheck;
 }): void {
@@ -94,22 +57,10 @@ export function performScopeAuthorizationCheck({
       "Member belongs to different organization"
     );
   }
-  const hasPermission = policy.scopes
-    .filter((scope) => tokenScopes.includes(scope.scope))
-    .flatMap((scope) => scope.permissions)
-    .some((permission) => {
-      const hasMatchingAction =
-        permission.actions.includes(authorizationCheck.action) ||
-        permission.actions.includes("*");
-      const hasMatchingResource =
-        authorizationCheck.resource_id === permission.resource_id;
-      return hasMatchingAction && hasMatchingResource;
-    });
-
-  if (!hasPermission) {
-    throw new ClientError(
-      "invalid_permissions",
-      "Member does not have permission to perform the requested action"
-    );
-  }
+  return performBaseScopeAuthorizationCheck({
+    policy,
+    tokenScopes,
+    authorizationCheck,
+    callerType: "Member",
+  });
 }
--- types/lib/b2c/rbac_local.d.ts
+++ types/lib/b2c/rbac_local.d.ts
@@ -1,21 +1,6 @@
-import { RBACPolicy, RBAC } from "./rbac";
-import { SessionsAuthorizationCheck } from "./sessions";
-export declare class PolicyCache {
-    private rbac;
-    private _policy?;
-    private _timestamp?;
-    constructor(rbac: RBAC);
-    private fresh;
-    private reload;
-    getPolicy(): Promise<RBACPolicy>;
+import { RBACPolicy } from "./rbac";
+import { BasePolicyCache, baseRoleAuthorizationCheckArgs, baseScopeAuthorizationCheckArgs } from "../shared/rbac_local";
+export declare class PolicyCache extends BasePolicyCache<RBACPolicy> {
 }
-export declare function performAuthorizationCheck({ policy, subjectRoles, authorizationCheck, }: {
-    policy: RBACPolicy;
-    subjectRoles: string[];
-    authorizationCheck: SessionsAuthorizationCheck;
-}): void;
-export declare function performScopeAuthorizationCheck({ policy, tokenScopes, authorizationCheck, }: {
-    policy: RBACPolicy;
-    tokenScopes: string[];
-    authorizationCheck: SessionsAuthorizationCheck;
-}): void;
+export declare function performAuthorizationCheck({ policy, subjectRoles, authorizationCheck, }: Omit<baseRoleAuthorizationCheckArgs, "callerType">): void;
+export declare function performScopeAuthorizationCheck({ policy, tokenScopes, authorizationCheck, }: Omit<baseScopeAuthorizationCheckArgs, "callerType">): void;
